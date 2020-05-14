e443422aa6c02a82c29037a4919598aee8695e6f
andi <aandi30082001@gmail.com>
2020-05-15 06:09:56 +0700

    initial commit

---
+++ assets/js/custom.js
@@ -0,0 +1,27 @@
+function homeFunction() {
+    var home = document.getElementById("home");
+    var pendidikan = document.getElementById("pendidikan");
+    var kontak = document.getElementById("kontak");
+    home.style.display = "block";
+    pendidikan.style.display = "none";
+    kontak.style.display = "none";
+}
+
+function pendidikanFunction() {
+    var home = document.getElementById("home");
+    var pendidikan = document.getElementById("pendidikan");
+    var kontak = document.getElementById("kontak");
+    home.style.display = "none";
+    pendidikan.style.display = "block";
+    kontak.style.display = "none";
+}
+
+function kontakFunction() {
+    var home = document.getElementById("home");
+    var pendidikan = document.getElementById("pendidikan");
+    var kontak = document.getElementById("kontak");
+    home.style.display = "none";
+    pendidikan.style.display = "none";
+    kontak.style.display = "block";
+}
+  
